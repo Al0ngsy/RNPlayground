c1ca1fdf23fa6c96c00e5303e9904bd8c6b99a79
Le Quoc Anh Tran <tranlequocanh@ymail.com>
2019-08-28 16:15:50 +0200

    Installed yarn and it did this
    ¯\_(ツ)_/¯

---
+++ components/ShowingDatas.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Platform, 
+        StyleSheet, 
+        Text, 
+        View,
+        TouchableOpacity,
+        AsyncStorage } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+import Constants from 'expo-constants';
+
+export default class SavingDatas extends Component {
+    static navigationOptions = {
+        title: 'Show Data',
+    };
+
+    
+}
